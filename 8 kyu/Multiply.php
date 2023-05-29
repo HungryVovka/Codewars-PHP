d02aae3286f422048702eb03a4488294ebfa8b51
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-05-29 23:51:16 +0500

    feat: 8 kyu solutions

--- 8 kyu/Multiply.php
+++ 8 kyu/Multiply.php
@@ -3,7 +3,40 @@
 // -----------------------------------------------------------
 
 <?php
-function multiply($a, $b){
-  return $a * $b;
+function solution($number){
+	$mult = 0;
+	for ($i = 0; $i < $number; $i++){
+		if ($i % 3 == 0){
+			$mult += $i;
+			continue;
+		} else if ($i % 5 == 0){
+			$mult += $i;
+			continue;
+		} else {
+			continue;
+		}
+	}
+	return $mult;
 }
 >
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
