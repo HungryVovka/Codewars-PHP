c020d8ed52a0443d15ba6da1c27fe50f44b53f8e
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-06-20 23:39:43 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Convert a Number to a String!.php
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------
+// We need a function that can transform a number (integer) into a string.
+// 
+// What ways of achieving this do you know?
+// 
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+// -----------------------------------------------------------
+
+<?php
+function numberToString($num){
+  return strval($num);
+}
+>
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
---
+++ 8 kyu/Even or Odd.php
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// -----------------------------------------------------------
+
+<?php
+function even_or_odd(int $n): string {
+  return ($n % 2 == 0) ? "Even" : "Odd";
+}
+>
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
