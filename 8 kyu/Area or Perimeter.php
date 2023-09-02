7f152508a8e29242c27c159e722d90964227dbfd
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-09-03 02:49:57 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Area or Perimeter.php
@@ -0,0 +1,42 @@
+<?php
+// -----------------------------------------------------------
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+// 
+// Example(Input1, Input2 --> Output):
+// 
+// 6, 10 --> 32
+// 3, 3 --> 9
+// 
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+// -----------------------------------------------------------
+
+function areaOrPerimeter(int $l, int $w){
+	if ($l == $w){
+		return $l * $w;
+	} else {
+		return 2 * $l + 2 * $w;
+	}
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
+>
---
+++ 8 kyu/Basic variable assignment.php
@@ -0,0 +1,30 @@
+<?php
+// -----------------------------------------------------------
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+// -----------------------------------------------------------
+
+$a = "code";
+$b = "wa.rs";
+$name = $a . $b;
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
+>
