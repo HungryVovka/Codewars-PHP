0055570e3ac35d557bcc00a1fdbb334d0ac32b11
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-09-02 23:00:33 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Function 1 - hello world.php
@@ -0,0 +1,34 @@
+<?php
+// -----------------------------------------------------------
+// Make a simple function called greet that returns the most-famous "hello world!".
+// 
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" 
+// solution you would want to show your friends?
+// -----------------------------------------------------------
+
+function greet(){
+	return "hello world!";
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2023 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-PHP
+// (https://github.com/HungryVovka/Codewars-PHP)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-PHP/blob/main/LICENSE.md)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
+>
